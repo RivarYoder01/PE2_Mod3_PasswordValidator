13f9f770ed93ef2f5da1ce817e0a413c8756634a
joniss01 <jonathan.nissen24@gmail.com>
2024-04-08 21:06:05 -0500

    added some documentation to password_exception

--- password_exception.py
+++ password_exception.py
@@ -1,9 +1,10 @@
 class PasswordException(Exception):
     def __init__(self, message, password):
         """
+        Initializes a new instance of PasswordException
 
-        :param message:
-        :param password:
+        :param message: A descriptive error message indicating the reason for the exception
+        :param password: The password associated with the validation error
         """
 
         Exception.__init__(self, message)
